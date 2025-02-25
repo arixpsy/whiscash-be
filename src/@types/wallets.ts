52453b46c7e5bd77e1ceecd67b09c2f9bff771d1
arixpsy <arixgg@gmail.com>
2025-02-26 01:07:37 +0800

    wip: update request types

--- src/@types/wallets.ts
+++ src/@types/wallets.ts
@@ -1,15 +1,19 @@
 import { z } from 'zod'
 import { SpendingPeriod } from '@/utils/enum'
 
-export const GetWalletsRequestSchema = z.object({
-  searchPhrase: z.string().optional(),
-  currency: z.string().length(3).optional(),
-})
-
 export const CreateWalletRequestSchema = z.object({
   name: z.string().min(1).max(50),
   currency: z.string().length(3),
   country: z.string().length(2),
   defaultSpendingPeriod: z.nativeEnum(SpendingPeriod),
   subWalletOf: z.number().optional(),
+})
+
+export const GetDashboardWalletsRequest = z.object({
+  timezone: z.string(),
+})
+
+export const GetWalletsRequestSchema = z.object({
+  searchPhrase: z.string().optional(),
+  currency: z.string().length(3).optional(),
 })
